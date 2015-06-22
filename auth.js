e9147b4ff8fef0aa7f850671f07d20c048fee625
alouis <alouis@MacBook-Pro.local>
2015-06-22 16:21:39 -0400

    Added necessary files

---
+++ auth.js
@@ -0,0 +1,9 @@
+var auth = {
+	facebook: {
+	 clientID: '937090636349988',
+	 clientSecret: 'd797a79d9d8be925477c86ec82872709',
+	 callbackURL: 'http://localhost:3000/auth/facebook/callback'		
+	}
+};
+
+module.exports = auth;
---
+++ views/index.jsx
@@ -0,0 +1,45 @@
+// tutorial1.js
+// Higher Level Architecutre
+// - CommentBox
+//   - CommentList
+//     - Comment
+//   - CommentForm
+
+
+var React = require('react');
+
+
+var CommentList = React.createClass({
+    render: function(){
+
+      var comments = this.props.data.map(function(c){
+        return(
+          <Comment author={c.author}>
+            {c.text}
+          </Comment>
+        );
+      });
+
+      return (
+          <div className="commentList" data={this.props.data}>
+            {comments}
+          </div>
+      );
+    }
+});
+
+var Comment = React.createClass({
+    render: function(){
+      var raw = this.props.children.toString();
+      return (
+          <div className="comment">
+            <h2 className="commentAuthor">
+              {this.props.author}
+            </h2>
+            <span dangerouslySetInnerHTML={{__html: raw}} />
+          </div>
+      );
+    }
+});
+
+module.exports = CommentList;
