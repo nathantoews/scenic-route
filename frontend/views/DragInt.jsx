4271dc52ee7cdb3b61626c0a982d17b64cb2d86e
nathantoews <nathantoews@gmail.com>
2015-07-09 12:12:17 -0400

    dragdemo

--- frontend/views/DragInt.jsx
+++ frontend/views/DragInt.jsx
@@ -1,38 +1,49 @@
 var React = require('react');
 
-function Rectangle (x, y, w, h, svgCanvas) {
-  this.svgCanvas = document.querySelector('.drag');
-  this.svgNS = 'http://www.w3.org/2000/svg';
-  this.rectangles = [];
-  this.x = x;
-  this.y = y;
-  this.w = w;
-  this.h = h;
-  this.stroke = 5;
-  this.el = document.createElementNS(this.svgNS, 'rect');
-  
-  this.el.setAttribute('data-index', this.rectangles.length);
-  this.el.setAttribute('class', 'edit-rectangle');
-  this.rectangles.push(this);
 
-  this.draw();
-  this.svgCanvas.appendChild(this.el);
-}
 
-Rectangle.prototype.draw = function () {
-  this.el.setAttribute('x', this.x + this.stroke / 2);
-  this.el.setAttribute('y', this.y + this.stroke / 2);
-  this.el.setAttribute('width' , this.w - this.stroke);
-  this.el.setAttribute('height', this.h - this.stroke);
-  this.el.setAttribute('stroke-width', this.stroke);
-}
+var TimeDrag = React.createClass({
+componentDidMount: function(){
 
-interact('.edit-rectangle')
+  var svgCanvas = document.querySelector('.drag');
+  var svgNS = 'http://www.w3.org/2000/svg';
+  var rectangles = [];
+
+  function Rectangle (x, y, w, h, svgCanvas) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.stroke = 5;
+    this.el = document.createElementNS(svgNS, 'rect');
+    
+    this.el.setAttribute('data-index', rectangles.length);
+    this.el.setAttribute('class', 'edit-rectangle');
+    rectangles.push(this);
+
+    this.draw();
+    svgCanvas.appendChild(this.el);
+  }
+
+  Rectangle.prototype.draw = function () {
+    this.el.setAttribute('x', this.x + this.stroke / 2);
+    this.el.setAttribute('y', this.y + this.stroke / 2);
+    this.el.setAttribute('width' , this.w - this.stroke);
+    this.el.setAttribute('height', this.h - this.stroke);
+    this.el.setAttribute('stroke-width', this.stroke);
+  }
+
+
+
+ interact('.edit-rectangle')
   // change how interact gets the
   // dimensions of '.edit-rectangle' elements
   .rectChecker(function (element) {
+    // console.log("Element if",);
+    // console.log("this", this);
+    // window.brrah = this;
     // find the Rectangle object that the element belongs to
-    var rectangle = this.rectangles[element.getAttribute('data-index')];
+    var rectangle = rectangles[element.getAttribute('data-index')];
 
     // return a suitable object for interact.js
     return {
@@ -58,7 +69,7 @@ interact('.edit-rectangle')
   .draggable({
     max: Infinity,
     onmove: function (event) {
-      var rectangle = this.rectangles[event.target.getAttribute('data-index')];
+      var rectangle = rectangles[event.target.getAttribute('data-index')];
 
       rectangle.x += event.dx;
       rectangle.y += event.dy;
@@ -68,7 +79,7 @@ interact('.edit-rectangle')
   .resizable({
     max: Infinity,
     onmove: function (event) {
-      var rectangle = this.rectangles[event.target.getAttribute('data-index')];
+      var rectangle = rectangles[event.target.getAttribute('data-index')];
 
       rectangle.w = Math.max(rectangle.w + event.dx, 10);
       rectangle.h = Math.max(rectangle.h + event.dy, 10);
@@ -77,13 +88,6 @@ interact('.edit-rectangle')
   });
 
 interact.maxInteractions(Infinity);
-
-
-
-
-var TimeDrag = React.createClass({
-
-componentDidMount: function(){
 
   new Rectangle(50, 80, 80, 80);
 
