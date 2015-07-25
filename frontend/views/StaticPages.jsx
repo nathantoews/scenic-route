004b4fe5dbc95f224ad0e274802c9b1358f5ee5e
Andrew Louis <alouis@criticalmass.com>
2015-07-25 18:44:35 -0400

    Added static pages file

---
+++ frontend/views/StaticPages.jsx
@@ -0,0 +1,48 @@
+var React = require('react/addons');
+var Classnames = require('classnames');
+var Navigate = require('../stores/Navigate.jsx');
+var Actions = require('../stores/Actions.jsx');
+var ScenicStore = require('../stores/Stores.jsx');
+
+var PageController = React.createClass({
+	getInitialState: function(){
+		return {
+			'activePage': null,
+			'containerCSS': Classnames('staticPageContainer','hide', ScenicStore.getLayout().containerMask)
+		};
+	},	
+	aboutUs: function(){
+		return (
+			<div className="viewContainer">
+				<h1>About Us</h1>
+				<hr />
+			</div>
+		);
+	},	
+	componentDidMount: function(){
+		window.hrr = this;
+		ScenicStore.addChangeListener(this.updateActivePage);
+	},
+	updateActivePage: function(){
+		var containerCSS; 
+		if (!ScenicStore.getActivePage())
+			containerCSS = Classnames('staticPageContainer','hide', ScenicStore.getLayout().containerMask);
+		else
+			containerCSS = Classnames('staticPageContainer', ScenicStore.getLayout().containerMask);
+		
+		this.setState({
+			'activePage': ScenicStore.getActivePage(),
+			'containerCSS': containerCSS
+		})
+	},		
+	render: function() {
+		return (
+		    <div className={this.state.containerCSS}>
+		    	{this.state.activePage ? this[this.state.activePage]() : null}
+		    </div>
+		);
+	}
+});
+
+module.exports = PageController;
+
